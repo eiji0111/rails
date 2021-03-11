35e0624adccbc5ced3532b4a4f28a8e72a56c09e
eiji0111 <ee111fender@gmail.com>
2021-03-11 12:08:06 +0000

    [Finish]2-4

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_10_112059) do
+ActiveRecord::Schema.define(version: 2021_03_10_112834) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
