8d9965225389673707fd5a850d4ec88c19b33372
eiji0111 <ee111fender@gmail.com>
2021-03-12 11:43:07 +0000

    [Finish]2-5

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -5,15 +5,21 @@ class TodolistsController < ApplicationController
 
   def create
     list = List.new(list_params)
-
     list.save
+    redirect_to todolist_path(list.id)
+  end
 
-    redirect_to'/top'
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
   end
 
   private
   def list_params
     params.require(:list).permit(:title, :body)
   end
-  
+
 end
